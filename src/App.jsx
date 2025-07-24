d37faf6c08575af56cf1173ec214677aa74085b4
Alina <alina220380@gmail.com>
2025-07-24 15:16:01 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import moviesFromServer from './api/movies.json';
+import movies from './api/movies.json';
 import './App.scss';
 import { MovieList } from './components/MovieList/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={moviesFromServer} />
+      <MovieList movies={movies} />
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
